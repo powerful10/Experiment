21ad4d7b552d61509967bf8e3f260197f10c15ce
Oybek Abdullayev <oa8315980@gmail.com>
2025-09-25 16:01:42 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <HashRouter>
       <div className={isDarkMode ? "dark-mode" : "light-mode"}>
         <header className="app-header container">
           <div className="left-section">
@@ -166,7 +166,7 @@ function App() {
 
         <Footer />
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
